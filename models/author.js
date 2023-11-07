f3b5616b9fee7580154f14451bc73626eb5db2c2
chihab1991 <chihab.chergui.1991@gmail.com>
2023-11-07 22:07:02 +0100

    First version of application moved into GitHub

---
+++ models/author.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema({
+	first_name: { type: String, required: true, maxLength: 100 },
+	family_name: { type: String, required: true, maxLength: 100 },
+	date_of_birth: { type: Date },
+	date_of_death: { type: Date },
+});
+
+AuthorSchema.virtual("name").get(function () {
+	let fullname = "";
+	if (this.first_name && this.family_name) {
+		fullname = `${this.family_name}, ${this.first_name}`;
+	}
+	return fullname;
+});
+
+AuthorSchema.virtual("url").get(function () {
+	return `/catalog/author/${this._id}`;
+});
+// AuthorSchema.virtual("date_of_birth_formatted").get(function () {
+// 	return DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+// 		DateTime.DATE_MED
+// 	);
+// });
+// AuthorSchema.virtual("date_of_death_formatted").get(function () {
+// 	return DateTime.fromJSDate(this.date_of_death).toLocaleString(
+// 		DateTime.DATE_MED
+// 	);
+// });
+AuthorSchema.virtual("lifespan_formatted").get(function () {
+	return DateTime.fromJSDate(
+		this.date_of_birth - this.date_of_death
+	).toLocaleString(DateTime.DATE_MED);
+});
+module.exports = mongoose.model("Author", AuthorSchema);
